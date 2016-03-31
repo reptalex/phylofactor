76c3dabbf24fda134008df4aee3b1406bd59e46c
reptalex <bigalculus@gmail.com>
2016-03-31 15:41:13 -0600

    fixed bug

--- R/residualVar.R
+++ R/residualVar.R
@@ -1,7 +1,3 @@
 residualVar <- function(prediction,Data){
   return(var(c(t(clr(t(Data))-clr(t(prediction))))))
-}residualVar <- function(prediction,Data){
-  return(var(c(t(clr(t(Data))-clr(t(prediction))))))
-}residualVar <- function(prediction,Data){
-  return(var(c(t(clr(t(Data))-clr(t(prediction))))))
 }
