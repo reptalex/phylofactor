de7e0e5f261190ed0d1f9cd248c03aac83ced243
reptalex <bigalculus@gmail.com>
2016-04-01 15:40:45 -0600

    Finds all tips spanned by a set of clades

--- R/uniqueTips.R
+++ R/uniqueTips.R
@@ -2,7 +2,7 @@ uniqueTips <- function(tree,sigClades){
   Clds <- length(sigClades)
   tips <- NULL
   for (cc in 1:Clds){
-    tips <- unique(c(tips,Descendants(tree,sigClades[cc],type='tips')[[1]]))
+    tips <- sort(unique(c(tips,Descendants(tree,sigClades[cc],type='tips')[[1]])))
   }
   return(tips)
 }
