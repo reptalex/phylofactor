ac30cffd6146846b7447f4088a25e6f4fa95707f
reptalex <bigalculus@gmail.com>
2016-04-05 19:01:19 -0600

    ...

--- R/OTUtoTaxa.R
+++ R/OTUtoTaxa.R
@@ -1,15 +1,9 @@
 OTUtoTaxa <- function(otus,Taxonomy,common.name=T){
 
-  if (typeof(otus)=='list'){
-    if (is.null(otus$group)==F)
-    taxa <- otus$group$IDs[,'TaxaIDs']
-    otus <- otus$group$IDs[,'otuIDs']
-  } else {
-    notus <- length(otus)
-    ind <- match(otus,Taxonomy[,1])
-    taxa <- sort(unlist(lapply(as.list(Taxonomy[ind,2]),FUN=toString)))
-  }
-  if(typeof(otus)!='character'){otus <- as.character(otus)}
+    if(typeof(otus)!='character'){otus <- as.character(otus)}
+      notus <- length(otus)
+      ind <- match(otus,Taxonomy[,1])
+      taxa <- sort(unlist(lapply(as.list(Taxonomy[ind,2]),FUN=toString)))
 
   if(common.name==T){
     return(substr(taxa[1], start = 1, stop = lcprefix(taxa[1], taxa[length(taxa)])))
