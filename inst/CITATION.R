450b44d69dcabefbc7e8470571343a7c55093c5d
Alex Washburne <bigalculus@gmail.com>
2017-02-05 15:11:38 -0500

    Citation & updated travis

---
+++ inst/CITATION.R
@@ -0,0 +1,10 @@
+## Thanks for using phylofactor!
+## To cite this package, please use the following BibTex entry:
+##
+##
+##
+## @Article{,
+##  title={Phylogenetic factorization of compositional data yields lineage-level associations in microbiome data},
+##  author={Washburne AD, Silverman JD, Leff JW, Bennett DJ, Darcy JL, Mukherjee S, Fierer N, and David LA},
+##  year={2017}
+##  }
