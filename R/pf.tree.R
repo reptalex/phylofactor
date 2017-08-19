6784293fa131e3e1a174f3cdd339ed37c3bf243e
Alex Washburne <bigalculus@gmail.com>
2017-08-18 20:03:26 -0600

    pftree

--- R/pf.tree.R
+++ R/pf.tree.R
@@ -8,7 +8,7 @@
 #' @param bg.color background color
 #' @param bg.alpha background alpha
 #' @param alphas vector, of length = \code{nrow(factor.map)} or \code{length(Grps)} or, if default settings, of length \code{pf$nfactors}. Controls alphas for highlights of each clade
-#' @param layout. See \code{\link{ggtree}}. Default is "circular"
+#' @param layout See \code{\link{ggtree}}. Default is "circular"
 #' @param rootnode Logical. If true, will fill in the root node. Default controls root only with bg.color 
 #' @param color.fcn Color function for plotting. Default is rainbow.
 #' @param ... optional arguments for ggtree
